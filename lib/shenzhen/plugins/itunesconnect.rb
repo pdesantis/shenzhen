6d3f83180bed92523f8327ccba5640e6a41d5fc1
Pat DeSantis <pdesantis3@gmail.com>
2015-04-15 13:25:42 -0700

    Update iTMSTransporter path for Xcode 6.3

--- lib/shenzhen/plugins/itunesconnect.rb
+++ lib/shenzhen/plugins/itunesconnect.rb
@@ -43,7 +43,7 @@ module Shenzhen::Plugins
 
       def transport
         xcode = `xcode-select --print-path`.strip
-        tool = File.join(File.dirname(xcode), "Applications/Application Loader.app/Contents/MacOS/itms/bin/iTMSTransporter").gsub(/\s/, '\ ')
+        tool = File.join(File.dirname(xcode), "Applications/Application Loader.app/Contents/itms/bin/iTMSTransporter").gsub(/\s/, '\ ')
 
         args = [tool, "-m upload", "-f Package.itmsp", "-u #{Shellwords.escape(@account)}", "-p #{Shellwords.escape(@password)}"]
         command = args.join(' ')
